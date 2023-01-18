17c77b7c0d5eaf944cf7988ebea8e52110252091
Paulina <paulina.justyna.niedzielska@gmail.com>
2023-01-18 18:02:23 +0000

    FizzBuzz

--- JS/app.js
+++ JS/app.js
@@ -9,3 +9,21 @@ function changeColor() {
  header.classList.toggle("redcolor")
 }
 welcomeBtn.addEventListener('click', changeColor)
+
+//fizzBuzz - using for loop, if else statement and template literals
+
+function fizzBuzz(lastNum) {
+ for (let i = 1; i < lastNum + 1; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+   console.log(`${i} - FizzBuzz`)
+  } else if (i % 3 === 0) {
+   console.log(`${i} - Fizz`)
+  } else if (i % 5 === 0) {
+   console.log(`${i} - Buzz`)
+  } else {
+   console.log(`${i}`)
+  }
+ }
+}
+
+// fizzBuzz(30);
