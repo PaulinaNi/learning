7b183140b7ed80c876e5827ff2f35247e0a98c58
Paulina N <paulina.justyna.niedzielska@gmail.com>
2023-02-03 19:39:42 +0000

    js for todo up

--- simpleToDo/app.js
+++ simpleToDo/app.js
@@ -1,15 +1,73 @@
 const addTaskBtn = document.getElementById('addTaskBtn')
 const addTaskSection = document.querySelector('.addTaskSection')
+const addTaskInput = document.querySelector('.addTaskInput')
+const taskListUL = document.querySelector('.taskListUL')
 
+const taskArray = []
+
+//remove all children from UL 
+const removeAllTasks = () => {
+ while (taskListUL.firstChild) {
+  taskListUL.firstChild.remove()
+ }
+}
+
+//function which will add all task from task array to DOM
+const addTasksToDOM = (task, index) => {
+ const taskLi = document.createElement('li')
+ taskLi.textContent = task
+
+ //add styles
+ // taskLi.classList.add(`${index}`)
+
+ //add delete button
+ const btn = createDeleteBTN(index)
+ taskLi.append(btn)
+
+ //appent task to DOM
+ taskListUL.append(taskLi)
+}
+
+//add all task to UL
+const addTasksToUL = () => {
+ taskArray.forEach((task, index) => addTasksToDOM(task, index))
+}
+
+//function to delete task
+const deleteTask = (index) => {
+ //delete task
+ taskArray.splice(index, 1)
+ //remove and add tasks again
+ removeAllTasks()
+ addTasksToUL()
+}
+
+//add btn to delete task when completed
+const createDeleteBTN = (index) => {
+ const deleteBTN = document.createElement('span')
+ deleteBTN.textContent = 'Delete'
+ deleteBTN.classList.add('deleteBTN')
+ deleteBTN.id = `${index}`
+ deleteBTN.addEventListener('click', () => deleteTask(index))
+ return deleteBTN
+}
+
+
+//add task handler
 const addTask = () => {
- const input = document.createElement('input')
 
- //input handling
- addTaskSection.append(input)
- input.addEventListener('input', (event) => {
-  event.preventDefault()
- })
+ //push task to array with all tasks 
+ taskArray.push(addTaskInput.value)
 
+ //clean input
+ addTaskInput.value = ''
+
+ //remove all task from dom
+ removeAllTasks()
+
+ //add all task to dom again with new task
+ addTasksToUL()
 }
 
 addTaskBtn.addEventListener('click', addTask)
+
